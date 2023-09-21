c8fac3abce2d63ef01c249d0c72d10d6773e01b8
cristhina <130621476+CRISTHINAPA@users.noreply.github.com>
2023-09-20 19:23:42 -0500

    tarea1
    taller

---
+++ tarea1/index.js
@@ -0,0 +1,9 @@
+//Declaramos la importación de lo que vamos a proceder a imprimir o usar.
+const {mostrarPacientesYControlRealizado,mostrarControlDeAlergias, IdUsuario,} = require('./EntidadesYFunciones')
+// Llamamos a las funciones que queremos imprimir
+
+//CICLO FOR
+mostrarPacientesYControlRealizado(idAlergias,Tratamiento, IdUsuario),
+//CICLO FOR..OF
+mostrarControlDeAlergias(Tratamiento, Fecha, Hora, Costo)
+//CICLO FOR EACH
